f3b7de877ed689791998d1683a743e3ec4c881b5
Your Name <you@example.com>
2022-08-14 21:33:18 +0200

    Refactor test to use rest template instead of restAssured

--- lib/src/test/java/dao/FirstTest.java
+++ lib/src/test/java/dao/FirstTest.java
@@ -3,11 +3,8 @@ package dao;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,18 +12,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import productservice.model.GetProductResponse;
 
 @SpringBootTest
@@ -46,20 +47,26 @@ public class FirstTest {
 	private static String AUTH_TOKEN = "";
 
 	@BeforeAll
-	public static void before() {
+	public static void before() throws JSONException {
 		// get access token
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("grant_type", "client_credentials");
-		params.put("client_secret", "gG3ID1HDikbvaHaa8kTnMhvHUQmginqw");
-		params.put("client_id", "login-app");
-
-		Response response = RestAssured.given().contentType("application/x-www-form-urlencoded").formParams(params)
-				.post(tokenUrl);
-		List<Integer> responses = Arrays.asList(404, 401, 400);
-		if (responses.contains(response.getStatusCode())) {
-			throw new RuntimeException("error: " + response.statusCode());
-		}
-		AUTH_TOKEN = response.jsonPath().getString("access_token");
+		RestTemplate restTemplate = new RestTemplate();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		
+		MultiValueMap<String, String> paramsForRestTemplate = new LinkedMultiValueMap<>();
+		
+		paramsForRestTemplate.add("grant_type", "client_credentials");
+		paramsForRestTemplate.add("client_secret", "gG3ID1HDikbvaHaa8kTnMhvHUQmginqw");
+		paramsForRestTemplate.add("client_id", "login-app");
+		
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(paramsForRestTemplate, headers);
+		
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(tokenUrl, request, String.class);
+		
+		JSONObject jsonObject = new JSONObject(responseEntity.getBody());
+		
+		AUTH_TOKEN = jsonObject.getString("access_token");
 	}
 
 	@Test
@@ -68,16 +75,16 @@ public class FirstTest {
 				.perform(get("/api/products/products").header(HttpHeaders.AUTHORIZATION, "Bearer " + AUTH_TOKEN))
 				.andExpect(status().isOk()).andReturn();
 
-		GetProductResponse response = mapObjects(result.getResponse().getContentAsString(), GetProductResponse.class);
-		Assertions.assertTrue( response.getProducts().size() == 3, "response should contain 3 elements");
+		GetProductResponse response = readJsonString(result.getResponse().getContentAsString(), GetProductResponse.class);
+		Assertions.assertTrue(response.getProducts().size() == 3, "response should contain 3 elements");
 	}
 
-	private static <T, S> T mapObjects(String jsonString, Class classOfObject) {
+	private static <T> T readJsonString(String jsonString, Class classOfObject) {
 
 		try {
 			return (T) mapper.readValue(jsonString, classOfObject);
 		} catch (JsonProcessingException e) {
-			throw new RuntimeException("Cannot read json class", e);
+			throw new RuntimeException("Cannot read json class, json string: " + jsonString, e);
 		}
 
 	}
