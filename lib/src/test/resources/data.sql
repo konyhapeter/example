267687ff3632337f93aa34c5835d7d735a23fb17
Your Name <you@example.com>
2022-08-14 19:11:23 +0200

    Add new files

---
+++ lib/src/test/resources/data.sql
@@ -0,0 +1,3 @@
+insert into product values(1, 'The Tartar Steppe', 'The Tartar Steppe', '123.2', 'asdasd');
+insert into product values(2, 'The description', 'The Steppe', '123.3', 'asdasd');
+insert into product values(3, 'The Tartar ', 'The Tartar ', '123.4', 'asdasd');
